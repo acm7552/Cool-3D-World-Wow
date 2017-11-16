c9cb9a566826b2d4edb1214eb33c04aea69fdc98
Dom <dgl6789@g.rit.edu>
2017-11-15 19:32:56 -0500

    Removed some unused materials, added water

---
+++ C3DW/Assets/scripts/Billboard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    Camera cam;
+
+    [SerializeField]
+    bool x, y, z;
+
+    // Use this for initialization
+    void Start() {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //Look at the camera.
+        Vector3 lookVector = cam.transform.position - transform.position;
+
+        Quaternion lookRotation = Quaternion.LookRotation(-lookVector);
+        transform.rotation = lookRotation;
+    }
+}
