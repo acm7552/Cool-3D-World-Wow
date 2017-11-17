d8f6f74c4c80044e0757aafcaeb7dd9faf4d04b7
Dom <dgl6789@g.rit.edu>
2017-11-16 20:31:48 -0500

    Finished terrain detailing
    Also recommitted some lib files because i guess gitignore is nothing more than a local cryptid

---
+++ C3DW/Assets/scripts/Atmosphere.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atmosphere : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    Color colorLow, colorHigh;
+
+    [SerializeField]
+    Vector2 bounds;
+
+    [SerializeField]
+    float maxDensity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float y = target.position.y;
+
+        float f = y / (bounds.y - bounds.x);
+
+        RenderSettings.fogDensity = f * maxDensity;
+        RenderSettings.fogColor = Color.Lerp(colorHigh, colorLow, f);
+	}
+}
