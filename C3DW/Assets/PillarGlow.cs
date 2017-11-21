67cfe2afbc900fd0063675d59d235429ced30540
Dominic Liotti (RIT Student) <dgl6789@ad.rit.edu>
2017-11-21 11:38:48 -0500

    Added pillar glowing framework

---
+++ C3DW/Assets/PillarGlow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarGlow : MonoBehaviour {
+
+    Animator anim;
+
+    [SerializeField]
+    PillarTrigger trigger;
+
+    bool lastTrigger;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(trigger.on != lastTrigger)
+        {
+            anim.SetBool("On", true);
+        }
+
+        lastTrigger = trigger.on;
+	}
+}
---
+++ C3DW/Assets/PillarTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarTrigger : MonoBehaviour {
+
+    public bool on;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+            on = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            on = false;
+    }
+}
