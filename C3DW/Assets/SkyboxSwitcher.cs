48370bf80dd27deffd28dce2ac3c32009f847781
dgl6789 <31418606+dgl6789@users.noreply.github.com>
2017-12-07 10:01:00 -0500

    fixed console errors, added skybox switcher

---
+++ C3DW/Assets/SkyboxSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxSwitcher : MonoBehaviour {
+
+    public Material[] skyboxes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player")
+        {
+            RenderSettings.skybox = skyboxes[1];
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            RenderSettings.skybox = skyboxes[0];
+        }
+    }
+}
